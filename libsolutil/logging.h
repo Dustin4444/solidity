885c2fe4c39af03acd1c6b10620d2c5983a67fc9
clonker <1685266+clonker@users.noreply.github.com>
2026-03-26 12:02:06 +0100

    add logging to libsolutil

---
+++ libsolutil/logging.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include <fmt/format.h>
+
+#include <atomic>
+#include <cstdio>
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+
+#if defined(_MSC_VER)
+#define SOL_NOINLINE __declspec(noinline)
+#elif defined(__GNUC__) || defined(__clang__)
+#define SOL_NOINLINE __attribute__((noinline))
+#else
+#define SOL_NOINLINE
+#endif
+
+namespace solidity
+{
+
+enum class LogLevel : uint8_t { trace = 0, debug = 1, info = 2, warn = 3, error = 4, off = 5 };
+
+/// Guard macro: skips argument evaluation when the logger's level is too low.
+///   SOL_LOG(logger, debug, "x={} y={}", expensive1(), expensive2());
+#define SOL_LOG(logger, lvl, ...) \
+    do { if ((logger).should_log(::solidity::LogLevel::lvl)) [[unlikely]] (logger).lvl(__VA_ARGS__); } while (0)
+
+class Logger {
+public:
+    explicit Logger(std::string name, LogLevel level = LogLevel::info, std::FILE* output = stdout)
+        : m_name(std::move(name)),
+          m_level(static_cast<uint8_t>(level)),
+          m_output(output) {}
+
+    [[nodiscard]] bool should_log(LogLevel level) const noexcept {
+        return static_cast<uint8_t>(level) >= *const_cast<volatile uint8_t const*>(&m_level);
+    }
+
+    void set_level(LogLevel level) noexcept {
+        m_level = static_cast<uint8_t>(level);
+    }
+
+    [[nodiscard]] LogLevel level() const noexcept {
+        return static_cast<LogLevel>(m_level);
+    }
+
+    [[nodiscard]] std::string const& name() const noexcept { return m_name; }
+
+    void set_output(std::FILE* output) noexcept { m_output = output; }
+
+    template <typename... Args>
+    void log(LogLevel const level, fmt::format_string<Args...> fmt, Args&&... args) const {
+        if (!should_log(level))
+            return;
+        fmt::print(m_output, fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    SOL_NOINLINE void trace(fmt::format_string<Args...> fmt, Args&&... args) const {
+        log(LogLevel::trace, fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    SOL_NOINLINE void debug(fmt::format_string<Args...> fmt, Args&&... args) const {
+        log(LogLevel::debug, fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    SOL_NOINLINE void info(fmt::format_string<Args...> fmt, Args&&... args) const {
+        log(LogLevel::info, fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    SOL_NOINLINE void warn(fmt::format_string<Args...> fmt, Args&&... args) const {
+        log(LogLevel::warn, fmt, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    SOL_NOINLINE void error(fmt::format_string<Args...> fmt, Args&&... args) const {
+        log(LogLevel::error, fmt, std::forward<Args>(args)...);
+    }
+
+private:
+    std::string m_name;
+    uint8_t m_level;
+    std::FILE* m_output;
+};
+
+class Registry {
+public:
+    static Registry& instance();
+
+    Logger& get(std::string const& name);
+    void set_level(std::string_view prefix, LogLevel level);
+    void set_output(std::FILE* output);
+    void clear();
+
+private:
+    Registry();
+    ~Registry();
+    struct Impl;
+    std::unique_ptr<Impl> m_impl;
+};
+
+std::optional<LogLevel> parseLogLevel(std::string_view s);
+
+}
